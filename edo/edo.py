21204e9fa62376e9d8580cd7e91ff7aefd9d822b
Pedro <pedroserpah@gmail.com>
2020-09-17 19:28:25 -0300

    adição da solução do e-3

--- edo/edo.py
+++ edo/edo.py
@@ -3,15 +3,19 @@ import numpy as np
 # y' = 2 * y + x + 1, y(x0) = y0
 # possui única solução pelo EXU
 
-x0, y0 = 1, 2
-h = 0.125 # tamanho do passo
+x0, y0 = 0, 2
+h = 0.1 # tamanho do passo
 n = 50
 
 def f(x, y):
-    return 2 * y + x + 1
+    return x**2+y+1
 
 def solucao_do_pvi(x):
-    return (1/4) * (-2 * x + 13 * np.exp(2 * x - 2) - 3)
+    return -x**2 - 2*x + 5 * np.exp(x) - 3
+
+print("Calculando a aproximação de Euler, Heun, Ralston e Serpson para y′=x2+y+1,y(0)=2.")
+print("O tamanho do gap entre os pontos é h=",h)
+print("O número de iteraçõe usado em cada método será n=",n)
 
 def euler(f, x0, y0, h):
     x = {0: x0}
@@ -48,7 +52,7 @@ def ralston(f, x0, y0, h):
         y[k + 1] = y[k] + (h / 3) * (1 * m1 + 2 * m2) # <- fórmula do método de Heun
     return x, y
 
-def ralston(f, x0, y0,c2, h):
+def rk2(f, x0, y0,c2, h):
     # c1 + c2 = 1 --> c1 = 1 - c2, c2 > 0
     c1 = 1 - c2
     x = {i: x0 + i * h for i in range(n)}
@@ -67,10 +71,22 @@ hxs, hys = heun(f, x0, y0, h)
 hx = [v for _, v in hxs.items()]
 hy = [v for _, v in hys.items()]
 
+rxs, rys = ralston(f, x0, y0, h)
+rx = [v for _, v in rxs.items()]
+ry = [v for _, v in rys.items()]
+
+
+print("Fazendo para qualquer método de ordem 2, com c2=1/99. Este método é conhecido na comunidade acadêmica de Matemática como Método Serpson")
+sxs, sys = rk2(f, x0, y0,1/99, h)
+sx = [v for _, v in sxs.items()]
+sy = [v for _, v in sys.items()]
+
 t = np.linspace(x0, x0 + n * h, 100)
 
 plt.scatter(x, y, label="Euler")
 plt.scatter(hx, hy, label="Heun")
+plt.scatter(rx, ry, label="Ralston")
+# plt.scatter(sx, sy, label="Serpson")
 plt.plot(t, solucao_do_pvi(t))
 plt.legend()
 plt.show()
